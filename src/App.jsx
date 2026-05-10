ebe39d2514e413ee992e390f5bb09a3cfc9d2c89
adetunjiewaoluwa-cmd <adetunjiewaoluwa@gmail.com>
2026-05-10 07:25:53 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,7 @@
-
+export default function App() {
+  return (
+    <div style={{color:"white",background:"black",minHeight:"100vh",display:"flex",alignItems:"center",justifyContent:"center",fontSize:"2rem"}}>
+      The Voryel is loading ✓
+    </div>
+  )
+}
